039cc4edb1212fc35a78f2b74ee26d4583ed43c4
Sohelrana2815 <sohel152302@gmail.com>
2024-10-21 07:13:26 +0600

    jwt token create and verify the token

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://stydy-flow.web.app",
+      "https://stydy-flow.firebaseapp.com",
+      "http://localhost:5173",
+    ],
+  })
+);
 
 // MongoDB  Connection String...
 
@@ -32,40 +40,81 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    const studyTaskCollection = client.db("STUDY_FLOW_DB").collection("tasks");
+    const studyTaskCollection = client
+      .db("STUDY_FLOW_DB")
+      .collection("studyTasks");
     const assignmentCollection = client
       .db("STUDY_FLOW_DB")
       .collection("assignments");
-    // Study Tasks Related Api
 
+    const submittedAssignmentCollection = client
+      .db("STUDY_FLOW_DB")
+      .collection("submittedAssignments");
+
+    const pendingAssignmentCollection = client
+      .db("STUDY_FLOW_DB")
+      .collection("pendingAssignments");
+
+    // ---------------------
+
+    // JWT TOKEN RELATED API
+
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // Verify Token Middleware
+
+    const verifyToken = (req, res, next) => {
+      const bearerToken = req.headers.authorization;
+      console.log(bearerToken);
+      if (!bearerToken) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // Study Tasks Related Api
     //task get api
-    app.get("/tasks", async (req, res) => {
+    app.get("/myStudyTasks", async (req, res) => {
       const email = req.query.email;
       // console.log("email from client", email);
       const query = { email: email };
-      console.log(query);
-
+      // console.log(query);
       const result = await studyTaskCollection.find(query).toArray();
       res.send(result);
     });
 
     // task  post api
 
-    app.post("/tasks", async (req, res) => {
+    app.post("/addStudyTask", async (req, res) => {
       const task = req.body;
       const result = await studyTaskCollection.insertOne(task);
       res.send(result);
     });
 
     // specific task get api
-    app.get("/tasks/:id", async (req, res) => {
+    app.get("/specificStudyTask/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await studyTaskCollection.findOne(filter);
       res.send(result);
     });
 
-    app.patch("/tasks/:id", async (req, res) => {
+    // Specific task update
+
+    app.patch("/updateStudyTask/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -79,7 +128,7 @@ async function run() {
 
     // specific task  delete api
 
-    app.delete("/tasks/:id", async (req, res) => {
+    app.delete("/deleteStudyTask/:id", async (req, res) => {
       const id = req.params.id;
 
       const filter = { _id: new ObjectId(id) };
@@ -88,10 +137,9 @@ async function run() {
     });
 
     //  ASSIGNMENTS RELATED API
+    // Get assignments by difficulty level
 
-    // GET API
-
-    app.get("/assignments", async (req, res) => {
+    app.get("/assignmentLevel", async (req, res) => {
       const difficultyLevel = req.query.difficultyLevel;
       const query = difficultyLevel ? { difficultyLevel } : {};
       // console.log(query);
@@ -99,16 +147,18 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/assignments/:id", async (req, res) => {
+    // SPECIFIC ASSIGNMENT GET API
+
+    app.get("/specificAssignment/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await assignmentCollection.findOne(filter);
       res.send(result);
     });
 
-    // PATCH
+    //SPECIFIC ASSIGNMENT UPDATE
 
-    app.patch("/assignments/:id", async (req, res) => {
+    app.patch("/updateAssignment/:id", async (req, res) => {
       const id = req.params.id;
       const assignment = req.body;
       console.log(assignment, id);
@@ -116,7 +166,7 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
 
       const updatedDoc = {
-        $set: { 
+        $set: {
           title: assignment.title,
           description: assignment.description,
           marks: assignment.marks,
@@ -129,9 +179,8 @@ async function run() {
       const result = await assignmentCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
-
-    // POST API
-    app.post("/assignments", async (req, res) => {
+    // Assignment post
+    app.post("/addAssignment", async (req, res) => {
       const assignment = req.body;
       console.log(assignment);
 
@@ -139,9 +188,81 @@ async function run() {
       res.send(result);
     });
 
-    // Get assignments by difficulty level
+    // DELETE API WILL COMING SOON....
 
-    // Delete assignment
+    // SUBMITTED ASSIGNMENT  API
+    // SPECIFIC USER SUBMITTED ASSIGNMENT
+
+    app.get("/mySubmittedAssignment", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      // console.log("Submitted Assignment user : ", email);
+      const query = { email: email };
+      // console.log("Submitted Assignment query", query);
+      const result = await submittedAssignmentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/submitAssignment", async (req, res) => {
+      const submittedAssignments = req.body;
+      // console.log(submittedAssignments);
+      const result = await submittedAssignmentCollection.insertOne(
+        submittedAssignments
+      );
+
+      console.log(result);
+      if (result.insertedId) {
+        // If successful, insert into the second collection
+        await pendingAssignmentCollection.insertOne(submittedAssignments);
+      }
+
+      res.send(result);
+    });
+    app.get("/specificSubmittedAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await submittedAssignmentCollection.findOne(filter);
+      res.send(result);
+    });
+
+    app.patch("/updateSpecificSubmittedAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const assignmentResult = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          obtainedMark: assignmentResult.obtainedMark,
+          feedback: assignmentResult.feedback,
+          status: assignmentResult.status,
+        },
+      };
+      const result = await submittedAssignmentCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+
+      console.log(result);
+      if (result.modifiedCount > 0) {
+        // Delete the assignment
+        await pendingAssignmentCollection.deleteOne(filter);
+      }
+      res.send(result);
+    });
+
+    app.delete("/deleteMarkedAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await submittedAssignmentCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // Pending Assignment apis
+
+    app.get("/pendingAssignments", verifyToken, async (req, res) => {
+      const result = await pendingAssignmentCollection.find().toArray();
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
